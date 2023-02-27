139bb4bb039804d9bb9e40e4879d573bfc54752f
RumitAshara <126247287+RumitAshara@users.noreply.github.com>
2023-02-27 19:42:29 +0530

    Update test.java

--- test.java
+++ test.java
@@ -1,7 +1,16 @@
+import java.util.Scanner;
 public class test
 {
 public static void main(String[]args)
     {
-        System.out.print("hello world");
+        Scanner sc = new Scanner();
+    
+        int a , b ;
+        System.out.print("enter value of a");
+        a = sc.nextInt();
+        System.out.print("enter value of a");
+        b = sc.nextInt();
+        b+=a
+        System.out.print("addition of a and b is "+b);
     }
 }    
